a6fb1094d2cc2bf63e12da811b1e84f5f5896894
Denis Bugaenko <d-bugaenko@rambler.com>
2021-06-27 21:05:47 +0500

    added rating

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import React from "react";
 
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
+import AppLoading from "expo-app-loading";
 
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { theme } from "./src/infrastructure/theme/index";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
-  return (
-    <>
-      <RestaurantsScreen />
-      <ExpoStatusBar style="auto" />
-    </>
-  );
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <ThemeProvider theme={theme}>
+          <RestaurantsScreen />
+        </ThemeProvider>
+        <ExpoStatusBar style="auto" />
+      </>
+    );
+  }
 }
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,24 +1,24 @@
 import React from "react";
 
-import { StyleSheet, View, StatusBar, SafeAreaView } from "react-native";
+import { StyleSheet, StatusBar, SafeAreaView } from "react-native";
 import { Searchbar } from "react-native-paper";
-import styled from "styled-components";
+import styled from "styled-components/native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  margin-top: ${StatusBar.currentHeight ?? 0}px;
 `;
+// ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
   flex: 1;
-  padding: 16px;
-  background-color: white;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => (
